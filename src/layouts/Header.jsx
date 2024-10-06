75b0ce7905c803d972c09eae86342dc6ca89b1bb
aimanshafi <shafiaiman88@gmail.com>
2024-10-07 00:26:13 +0600

    feat: supbase setup, clerk theme add, homepage design

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -45,9 +45,10 @@ export default function Header() {
 
           {showSignIn && (
             <div
-              className="fixed inset-0 flex items-center justify-center bg-slate-900/80"
+              className="fixed inset-0 flex items-center justify-center bg-slate-900/80 z-50"
               onClick={handleOverlayClick}
             >
+              {/* TODO: fix redirect URL */}
               <SignIn
               // signUpForceRedirectUrl="/onboard"
               // signInFallbackRedirectUrl="/onboard"
