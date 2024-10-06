ea0e34ae8b79f2e619d2cb1e61a4282d62de8a13
aimanshafi <shafiaiman88@gmail.com>
2024-10-06 23:13:56 +0600

    feat: auth added on header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,11 +1,43 @@
 // import { ModeToggle } from "@/components/mode-toggle";
 
 import ThemeToggle from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, UserButton, SignIn } from "@clerk/clerk-react";
+import { useState } from "react";
 
 export default function Header() {
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+    }
+  };
+
   return (
-    <div>
-      <ThemeToggle />
-    </div>
+    <header className="py-10">
+      <div className="flex justify-between max-w-6xl mx-auto">
+        <div className="logo font-thin text-2xl">JOBREX</div>
+        <div>
+          <ThemeToggle />
+          <SignedOut>
+            <Button onClick={() => setShowSignIn(true)}>Login</Button>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+
+          {showSignIn && (
+            <div
+              className="fixed inset-0 flex items-center justify-center bg-slate-900/80"
+              onClick={handleOverlayClick}
+            >
+              <SignIn />
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
   );
 }
